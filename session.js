ec06bd5f68db69da1e3bbc0093317dc55c5be107
parthoxrs <contact.nxt.media@gmail.com>
2025-02-06 17:33:12 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~nVFRhIrQ#DkioVwap6gQPY4uAYT-zLHgmuIhmHpUc2tjfODWZidI",
 PORT: process.env.PORT || "8000"
 };
